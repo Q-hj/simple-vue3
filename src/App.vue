8d78db8bc0b87f2139c72b0d4733d58aeb042fac
LuckyCat <1485642186@qq.com>
2022-11-07 10:39:37 +0800

    :lipstick:

--- src/App.vue
+++ src/App.vue
@@ -5,111 +5,117 @@
  * @Description: 
 -->
 <script setup>
-	import myMap from './components/myMap.vue';
-	import locationSvg from './assets/svg/location.vue';
-	import { ref, watch, onMounted } from 'vue';
-	import { get, post } from './apis/http';
-	const mapEl = ref(null);
+import myMap from "./components/myMap.vue";
+import locationSvg from "./assets/svg/location.vue";
+import { ref, watch, onMounted } from "vue";
+import { get, post } from "./apis/http";
+const mapEl = ref(null);
 
-	let venueList = ref([]);
-	onMounted(async () => {
-		venueList.value = await get('/pavilion/select');
+let venueList = ref([]);
+onMounted(async () => {
+  const list = await get("/pavilion/select");
 
-		mapEl.value.addMaker(venueList.value);
-	});
+  venueList.value = list.sort((a, b) => b.id - a.id);
 
-	const handleClick = ({ path }) => window.open(path);
+  mapEl.value.addMaker(venueList.value);
+});
+
+const handleClick = ({ path }) => window.open(path);
 </script>
 
 <template>
-	<div class="container">
-		<section class="left">
-			<myMap ref="mapEl"></myMap>
-		</section>
+  <div class="container">
+    <section class="left">
+      <myMap ref="mapEl"></myMap>
+    </section>
 
-		<section class="right">
-			<header class="h-50px">
-				<p class="text-lg font-bold text-yellow-600">浙江省</p>
-			</header>
-			<main class="border-2 border-yellow-500/50">
-				<li
-					class="h-1/5 between-center px-30px box-border"
-					v-for="(item, index) in venueList"
-					:key="index"
-				>
-					<aside class="between-center">
-						<div class="rounded-1/2 w-45px h-45px border border-orange-300 p-8px flex-center">
-							<locationSvg class="fill-yellow-400"></locationSvg>
-						</div>
-						<article class="col-between ml-10px h-full">
-							<b>{{ item.name }}</b>
-							<span class="text-sm text-gray-600 max-w-300px">{{ item.location }}</span>
-						</article>
-					</aside>
-					<button
-						@click="handleClick(item)"
-						v-show="item.path"
-						class="btn"
-						:class="index == 1 ? 'active' : ''"
-					>
-						<p class="w-90% border-transparent">查看</p>
-					</button>
-				</li>
-			</main>
-		</section>
-	</div>
+    <section class="right">
+      <header class="h-50px">
+        <p class="text-lg font-bold text-yellow-600">浙江省</p>
+      </header>
+      <main class="border-2 border-yellow-500/50">
+        <li
+          class="h-1/5 between-center px-30px box-border"
+          v-for="(item, index) in venueList"
+          :key="index"
+        >
+          <aside class="between-center">
+            <div
+              class="rounded-1/2 w-45px h-45px border border-orange-300 p-8px flex-center"
+            >
+              <locationSvg class="fill-yellow-400"></locationSvg>
+            </div>
+            <article class="col-between ml-10px h-full">
+              <b>{{ item.name }}</b>
+              <span class="text-sm text-gray-600 max-w-300px">{{
+                item.location
+              }}</span>
+            </article>
+          </aside>
+          <button
+            @click="handleClick(item)"
+            v-show="item.path"
+            class="btn"
+            :class="index == 1 ? 'active' : ''"
+          >
+            <p class="w-90% border-transparent">查看</p>
+          </button>
+        </li>
+      </main>
+    </section>
+  </div>
 </template>
 
 <style scoped>
-	.btn {
-		@apply btn font-bold px-7 py-3px transition duration-200 text-red-700 rounded-none border border-red-700 hover: text-yellow-300 hover:bg-red-700 hover:all:border-yellow-700;
-	}
+.btn {
+  @apply btn font-bold px-7 py-3px transition duration-200 text-red-700 rounded-none border border-red-700 hover: text-yellow-300 hover:bg-red-700 hover:all:border-yellow-700;
+}
 
-	@variants hover {
-	}
+@variants hover {
+}
 </style>
 <style scoped lang="less">
-	@li-py: 12px;
+@li-py: 12px;
 
-	.container {
-		width: 1200px;
-		border-radius: 30px;
-		padding: 30px;
-		background-color: #fff;
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		// grid-template-rows: repeat(2, 1fr);
-		gap: 50px;
+.container {
+  width: 1200px;
+  border-radius: 30px;
+  padding: 30px;
+  background-color: #fff;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  // grid-template-rows: repeat(2, 1fr);
+  gap: 50px;
 
-		section {
-			&.left {
-				background-color: rgb(221, 55, 55);
-				border-radius: 10px;
-			}
+  section {
+    &.left {
+      background-color: rgb(221, 55, 55);
+      border-radius: 10px;
+    }
 
-			&.right {
-				main {
-					// height: calc(100% - 50px);
-					height: calc(600px - 60px);
-					overflow: scroll;
+    &.right {
+      main {
+        // height: calc(100% - 50px);
+        height: calc(600px - 60px);
+        overflow: scroll;
 
-					&::-webkit-scrollbar {
-						display: none;
-					}
+        &::-webkit-scrollbar {
+          display: none;
+        }
 
-					li {
-						padding-top: @li-py;
-						padding-bottom: @li-py;
+        li {
+          padding-top: @li-py;
+          padding-bottom: @li-py;
 
-						aside {
-							height: calc(100% - (@li-py * 2));
+          aside {
+            height: calc(100% - (@li-py * 2));
 
-							article {
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+            article {
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
--- src/components/myMap.vue
+++ src/components/myMap.vue
@@ -5,74 +5,78 @@
  * @Description: 
 -->
 <script setup>
-	import { ref, watch, onMounted } from 'vue';
-	import { get, post } from '../apis/http';
-	let map;
+import { ref, watch, onMounted } from "vue";
+import { get, post } from "../apis/http";
+let map;
 
-	onMounted(async () => {
-		initMap();
-	});
+onMounted(async () => {
+  initMap();
+});
 
-	const makerWindow = new T.InfoWindow();
+const makerWindow = new T.InfoWindow();
 
-	const setMakerWindow = ({ name }, target) => {
-		makerWindow.setContent(
-			"<div> <div class='p-t-5  makerWindow'>" +
-				`<p class='fz-16 c-white tac'>${name}</p>` +
-				`<p class='fz-16 c-white tac'>热点数量：${0}</p>` +
-				'' +
-				'</div></div>'
-		);
-		target.openInfoWindow(makerWindow);
-	};
+const setMakerWindow = ({ name, repositoryNum }, target) => {
+  makerWindow.setContent(
+    "<div> <div class='p-t-5  makerWindow'>" +
+      `<p class='fz-16 c-white tac'>${name}</p>` +
+      `<p class='fz-16 c-white tac'>资源数量：${repositoryNum}</p>` +
+      "" +
+      "</div></div>"
+  );
+  target.openInfoWindow(makerWindow);
+};
 
-	//
-	const imageURL =
-		'http://t0.tianditu.gov.cn/img_w/wmts?' +
-		'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles' +
-		'&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=cbf747f3c05f4a8ce26160b19ffe581a';
+//
+const imageURL =
+  "http://t0.tianditu.gov.cn/img_w/wmts?" +
+  "SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles" +
+  "&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=cbf747f3c05f4a8ce26160b19ffe581a";
 
-	// 实例化地图
-	const initMap = () => {
-		if (!T) return setTimeout(() => initMap(), 200);
-		map = new T.Map('map-canvas');
-		map.centerAndZoom(new T.LngLat(120.170667, 30.274543), 8); // 设置中心点和比例
-		// map.addLayer(new T.TileLayer(imageURL, { minZoom: 1, maxZoom: 18 }));
-	};
-	//向地图上添加标注
-	const addMaker = (list) => {
-		// console.log(list);
+// 实例化地图
+const initMap = () => {
+  if (!T) return setTimeout(() => initMap(), 200);
+  map = new T.Map("map-canvas");
+  map.centerAndZoom(new T.LngLat(120.170667, 30.274543), 8); // 设置中心点和比例
+  // map.addLayer(new T.TileLayer(imageURL, { minZoom: 1, maxZoom: 18 }));
+};
+//向地图上添加标注
+const addMaker = (list) => {
+  // console.log(list);
 
-		list.forEach(({ longitude, latitude, legendUrl, ...other }) => {
-			const maker = new T.Marker(new T.LngLat(+longitude, +latitude), getMarkerOptions(legendUrl));
-			maker.on('mouseover', ({ lnglat, target, originalEvent }) => {
-				setMakerWindow(other, target);
-			});
-			map.addOverLay(maker);
-		});
-	};
+  list.forEach(({ longitude, latitude, legendUrl, ...other }) => {
+    const maker = new T.Marker(
+      new T.LngLat(+longitude, +latitude),
+      getMarkerOptions(legendUrl)
+    );
+    maker.on("mouseover", ({ lnglat, target, originalEvent }) => {
+      setMakerWindow(other, target);
+    });
+    map.addOverLay(maker);
+  });
+};
 
-	const defultIcon = new URL(`/src/assets/icon/location.png`, import.meta.url).href;
+const defultIcon = new URL(`/src/assets/icon/location.png`, import.meta.url)
+  .href;
 
-	const getMarkerOptions = (iconUrl = defultIcon) =>
-		iconUrl
-			? {
-					icon: new T.Icon({ iconUrl, iconSize: new T.Point(40, 40) }),
-			  }
-			: null;
-	defineExpose({
-		addMaker,
-	});
+const getMarkerOptions = (iconUrl = defultIcon) =>
+  iconUrl
+    ? {
+        icon: new T.Icon({ iconUrl, iconSize: new T.Point(40, 40) }),
+      }
+    : null;
+defineExpose({
+  addMaker,
+});
 </script>
 
 <template>
-	<div style="width: 100%; height: 100%">
-		<div
-			class="map-canvas"
-			id="map-canvas"
-			style="width: 100%; height: 100%"
-		></div>
-	</div>
+  <div style="width: 100%; height: 100%">
+    <div
+      class="map-canvas"
+      id="map-canvas"
+      style="width: 100%; height: 100%"
+    ></div>
+  </div>
 </template>
 
 <style lang="less" scoped></style>
